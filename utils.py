c610cba845d874db8b7b6876fb19a2e0fdb754ec
rmb4kt <bojilkata2@mail.bg>
2023-02-02 14:00:31 +0200

    added naive bayes

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import re
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+
+# nltk.download("stopwords")
+# nltk.download("wordnet")
 
 NON_ENGLISH_SYMBOLS = "À|Á|Â|Ã|Ä|Å|Æ|Ç|È|É|Ê|Ë|Ì|Í|Î|Ï|Ð|Ñ|Ò|Ó|Ô|Õ|Ö|×|Ø|Ù|Ú|Û|Ü|Ý|Þ|ß|à|á|â|ã|ä|å|æ|ç|è|é|ê|ë|ì|í|î|ï|ð|ñ|ò|ó|ô|õ|ö|÷|ø|ù|ú|û|ü|ý|þ|ÿ"
 
